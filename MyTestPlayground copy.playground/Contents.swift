83deb774414a63234c07d400c398d49d44661a1e
Noah Franck <ultimatedeath11@icloud.com>
2017-05-14 23:44:36 -0500

    uploaded files

---
+++ MyTestPlayground copy.playground/Contents.swift
@@ -0,0 +1,18 @@
+
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+
+var a = 5
+var b = 1
+
+if a+b ? 8{
+    print("true")
+}else{
+    print("false")
+}
+
